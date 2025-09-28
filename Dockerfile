3a4f93ffe1bb850cbf3e5ce1ce2ab922a60ea3bf
Peter Griffin <joe444824@gmail.com>
2025-09-29 03:44:07 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Step 1: Build stage
+FROM node:18-alpine AS build
+
+WORKDIR /app
+
+# Copy package files from src directory
+COPY package*.json ./
+RUN npm install
+
+# Copy only the src directory for build
+COPY . .
+RUN npm run build
+
+# Step 2: Serve with Nginx
+FROM nginx:stable-alpine
+
+COPY --from=build /app/dist /usr/share/nginx/html
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Experience from "./sections/WorkExperience";
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
+import Footer from "./sections/Footer";
+import Certificates from "./sections/Certificate";
+import Contact from "./sections/Contact";
+
+// TestTestgit 
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Experience />
+      <Certificates />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
